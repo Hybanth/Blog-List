d216e2270dcc2a1bb9060b4e4e1d140a38297be2
hybanth <hybanth@gmail.com>
2025-11-02 19:59:50 +0530

    Separated logger utility into its own module

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 require('dotenv').config()
+const logger = require('./utils/logger')
 
 const app = express()
 
@@ -34,5 +35,5 @@ app.post('/api/blogs', (request, response) => {
 
 const PORT = 3003
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  logger.info(`Server running on port ${PORT}`)
 })
